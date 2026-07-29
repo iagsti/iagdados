0b9a5ff13b4eff8fd1efe6cedaedffd5366fdccd
Marcelo Schneider <schneider.fei@gmail.com>
2026-07-29 16:05:41 -0300

    Corrige ip do proxy para o horus

--- src/iag/defs/horus/definitions.py
+++ src/iag/defs/horus/definitions.py
@@ -12,7 +12,7 @@ def resources():
                 base_url=dg.EnvVar("HORUS_BASE_URL"), 
                 username=dg.EnvVar("HORUS_USERNAME"),
                 password=dg.EnvVar("HORUS_PASSWORD"),
-                proxy_server="http://10.70.1.8"
+                proxy_server="http://10.70.1.8:3128"
             ),
             "horus_target": SqlAlchemyResource(
                 connection_string=dg.EnvVar("MARIADB_CONNECTION_STRING")
