6d1d6a41804944e5892bdea53029c234e94f6871
Marcelo Schneider <schneider.fei@gmail.com>
2026-08-07 16:20:05 -0300

    Adiciona composer para produção

---
+++ src/iag/defs/acesso/assets.py
@@ -0,0 +1,28 @@
+import dagster as dg
+import pandas as pd
+from ..resources import SqlAlchemyResource
+
+
+@dg.asset()
+def pessoa_raw(trino_resource: SqlAlchemyResource) -> pd.DataFrame:
+    """
+    Asset que representa a tabela 'pessoa' no banco de dados.
+
+    :param trino_resource: Recurso para conexão com o banco de dados
+    :return: DataFrame contendo os dados da tabela 'pessoa'
+    """
+    query = "SELECT * FROM pessoa_info"
+    query = """
+    SELECT
+        codpes,
+        nome,
+        dt_nasc,
+        sexo,
+        email,
+        telefone
+    FROM pessoa_info
+    """
+    engine = trino_resource.get_engine()
+    with engine.connect() as connection:
+        df = pd.read_sql(query, connection)
+    return df
